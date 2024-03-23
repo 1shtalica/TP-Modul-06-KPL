d6db001228b46ac5e25fefac7ec9f063633006a5
1shtalica <adibfaizulhaq05@gmail.com>
2024-03-23 20:18:41 +0700

    Initialize this repository

---
+++ TP Modul 06_1302223110_Adib Faizulhaq Armadhani/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Modul_06_1302223110_Adib_Faizulhaq_Armadhani
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
